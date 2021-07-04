3b9ff2eca9c1f5a4a79efeebdb06f23b84b7e736
ImGabreuw <imgabreuw.dev@gmail.com>
2021-07-04 18:01:16 -0300

    Trabalhando com templates HTML

--- teste.go
+++ teste.go
@@ -1,13 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+type Task struct {
+	Name string
+	Done bool
+}
 
 func main() {
 
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/tasks", TaskHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request)  {
 	w.Write([]byte("Olá Mundo"))
+}
+
+func TaskHandler(w http.ResponseWriter, r *http.Request)  {
+	task := Task{
+		Name: "Aprender Go",
+		Done: true,
+	}
+
+	t := template.Must(template.ParseFiles("task.html"))
+	t.Execute(w, task)
 }
