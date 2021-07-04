6a7af44c63b27bbe3b17d4dd68f340091883acc0
ImGabreuw <imgabreuw.dev@gmail.com>
2021-07-04 17:54:40 -0300

    Exemplo simples de um Web Server

--- teste.go
+++ teste.go
@@ -1,38 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
-
-type vehicle interface {
-	start() string
-}
-
-type Car struct {
-	CarName string `json:"car"`
-	CarYear int    `json:"year"`
-}
-
-func (car Car) start() string {
-	return "Iniciou"
-}
-
-func (car Car) drive() {
-	fmt.Println(car.CarName, "andou!")
-}
-
-func acceptOnlyVehicleImplementation(car vehicle) {
-	fmt.Println("É um veículo")
-}
+import "net/http"
 
 func main() {
 
-	car := Car{
-		CarName: "Fusion",
-		CarYear: 2020,
-	}
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+}
 
-	car.drive()
-	acceptOnlyVehicleImplementation(car)
-
+func Hello(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Olá Mundo"))
 }
